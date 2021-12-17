805489c7788f7f500715c77f06f64120a77f498f
Evgeniy <epchelinov@gmail.com>
2021-12-17 19:24:22 +1000

    создает компонент для ссылок навигации

--- src/components/BaseNavigation.vue
+++ src/components/BaseNavigation.vue
@@ -1,6 +1,8 @@
 <script>
 import { ROUTES } from "@/router/routes";
+import BaseNavigationLink from "./BaseNavigationLink.vue";
 export default {
+  components: { BaseNavigationLink },
   data() {
     return {
       admin: ROUTES.Admin,
@@ -11,39 +13,33 @@ export default {
 </script>
 
 <template>
-  <nav class="grid gap-2 content-center <md:grid-flow-col md:w-60 p-5 bg-white rounded-xl shadow-sm">
+  <nav
+    class="grid gap-2 content-center <md:grid-flow-col md:w-60 p-5 bg-white rounded-xl shadow-sm"
+  >
     <router-link
       :to="admin"
-      class="link"
-      v-slot="{ href, navigate, isExactActive }"
+      v-slot="{ href, navigate, isActive }"
     >
-      <template>
-        <a
-          :href="href"
-          @click="navigate"
-          :class="{ 'text-white bg-primary': isExactActive }"
-        >Админ</a>
-      </template>
+      <base-navigation-link
+        :href="href"
+        :navigate="navigate"
+        :is-active="isActive"
+      >
+        Админ
+      </base-navigation-link>
     </router-link>
 
     <router-link
       :to="user"
-      class="link"
       v-slot="{ href, navigate, isActive }"
     >
-      <template>
-        <a
-          :href="href"
-          @click="navigate"
-          :class="{ 'text-white bg-primary': isActive }"
-        >Пользователь</a>
-      </template>
+      <base-navigation-link
+        :href="href"
+        :navigate="navigate"
+        :is-active="isActive"
+      >
+        Пользователь
+      </base-navigation-link>
     </router-link>
   </nav>
 </template>
-
-<style scoped>
-.link {
-  @apply px-4 py-3 rounded-lg transition-colors duration-200 hover:(bg-gray-100);
-}
-</style>
---
+++ src/components/BaseNavigationLink.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      required: true,
+    },
+    navigate: {
+      type: Function,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <a
+    :href="href"
+    @click="navigate"
+    class="px-4 py-3 rounded-lg transition-colors duration-200 hover:(bg-gray-100)"
+    :class="{ 'text-white bg-primary': isActive }"
+  ><slot>Ссылка</slot></a>
+</template>
