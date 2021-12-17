877aee0bf812606b1d7729c9c942566835483d5a
Evgeniy <epchelinov@gmail.com>
2021-12-17 19:27:27 +1000

    исправляет компонент ссылок навигации

--- src/components/BaseNavigation.vue
+++ src/components/BaseNavigation.vue
@@ -19,6 +19,7 @@ export default {
     <router-link
       :to="admin"
       v-slot="{ href, navigate, isActive }"
+      custom
     >
       <base-navigation-link
         :href="href"
@@ -32,6 +33,7 @@ export default {
     <router-link
       :to="user"
       v-slot="{ href, navigate, isActive }"
+      custom
     >
       <base-navigation-link
         :href="href"
